3d4ae8fae5139cff31941ccfbce0e48e94d96ea8
PinalBhatt <self@pbdesk.com>
2013-09-29 00:08:44 -0400

    commit

--- PBDesk.JSUtils/PBDesk.JSUtils/jasmine.tests/spec/PBDeskJS.Utils.Spec.js
+++ PBDesk.JSUtils/PBDesk.JSUtils/jasmine.tests/spec/PBDeskJS.Utils.Spec.js
@@ -114,42 +114,171 @@ describe("PBDeskJS.Utils", function () {
 
     describe("Clone() Method", function () {
 
-        it("Non Object - Numeric Value", function () {
-            var actual = 1;
-            var clone = PBDeskJS.Utils.Clone(actual);
-            expect(clone).toEqual(actual);
+        describe("Input as non-object or null", function () {
+            it("Non Object - Numeric Value", function () {
+                var actual = 1;
+                var clone = PBDeskJS.Utils.Clone(actual);
+                expect(clone).toEqual(actual);
+            });
+
+            it("Non Object - String Value", function () {
+                var actual = '1';
+                var clone = PBDeskJS.Utils.Clone(actual);
+                expect(clone).toEqual(actual);
+            });
+
+            it("Null Object", function () {
+                var actual = '1';
+                var clone = PBDeskJS.Utils.Clone(actual);
+                expect(clone).toEqual(actual);
+            });
+
         });
 
-        it("Non Object - String Value", function () {
-            var actual = '1';
-            var clone = PBDeskJS.Utils.Clone(actual);
-            expect(clone).toEqual(actual);
-        });
+        describe("Input Object of Date Type", function () { 
 
-        it("Null Object", function () {
-            var actual = '1';
-            var clone = PBDeskJS.Utils.Clone(actual);
-            expect(clone).toEqual(actual);
-        });
-
-        describe("Date as  input Object", function () { 
-
-            it("Non Clone", function () {
-                var obj1 = Date();
+            xit("Non Clone", function () {
+                var obj1 = new Date();
                 var obj2 = obj1;
-                expect(obj2).toEqual(obj1);
+                //expect(obj2).toEqual(obj1);
                 expect(obj2).toBe(obj1);
-                obj1 = Date(2013, 10, 1);
-                expect(obj2).toEqual(obj1);
+                obj1 = new Date(2013, 1, 1);
+                //expect(obj2).toEqual(obj1);
                 expect(obj2).toBe(obj1);
             })
 
-            it("Date Object", function () {
-                var actual = Date();
+            it("acutal == clone", function () {
+                var actual = new Date();
                 var clone = PBDeskJS.Utils.Clone(actual);
                 expect(clone).toEqual(actual);
-                actual = actual + 1;
+                
+            });
+
+            it("actual === clone", function () {
+                var actual = new Date();
+                var clone = PBDeskJS.Utils.Clone(actual);
+
+                expect(clone).not.toBe(actual);
+
+            });
+
+            it("clone != actual", function () {
+                var actual = new Date(2013,1,1);
+                var clone = PBDeskJS.Utils.Clone(actual);
+                actual = actual.setYear(2012);
                 expect(clone).toNotEqual(actual);
+            });
+
+        });
+
+        describe("Input object of Array Type", function () {
+
+            describe("Empty Array Test", function () {
+                it("=== check", function () {
+                    var actual = new Array();
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone).not.toBe(actual);
+                });
+
+                it("Length check", function () {
+                    var actual = new Array();
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone.length).toBe(0);
+                });
+            });
+
+            describe("Misc. Tests", function () {
+                it("Changing Original Array after cloning - Length Test", function () {
+                    var actual = new Array();
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    actual[0] = "test";
+                    expect(clone.length).toBe(0);
+                });
+            });
+
+            describe("Cloning array of size 1", function () {
+                it("Length Test", function () {
+                    var actual = new Array();
+                    actual[0] = "test";
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone.length).toBe(1);
+                });
+
+                it("Value Test", function () {
+                    var actual = new Array();
+                    actual[0] = "test";
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone[0]).toEqual("test");
+                });
+
+                it("Value Test Case-2", function () {
+                    var actual = new Array();
+                    actual[0] = "test";
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone[0]).toEqual(actual[0]);
+                });
+
+                it("Changing Value after cloning.", function () {
+                    var actual = new Array();
+                    actual[0] = "alpha";
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    actual[0] = "beta";
+                    expect(clone[0]).toEqual("alpha");
+                });
+
+                it("Changing Value after cloning. Negative Test", function () {
+                    var actual = new Array();
+                    actual[0] = "alpha";
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    actual[0] = "beta";
+                    expect(clone[0]).not.toEqual(actual[0]);
+                });
+            });
+           
+            describe("Cloning array of size 2", function () {
+
+                var actual = [];
+
+                beforeEach(function () {
+                    actual = ['abc', 'def'];
+                });
+
+                afterEach(function () {
+                    actual = [];
+                });
+
+                it("Length Test", function () {
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone.length).toBe(2);
+                });
+
+                it("Value Test", function () {
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone[1]).toEqual("def");
+                });
+
+                it("Value Test Case-2", function () {
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    expect(clone[1]).toEqual(actual[1]);
+                });
+
+                it("Changing Value after cloning.", function () {
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    actual[0] = "ghi";
+                    expect(clone[1]).toEqual("def");
+                });
+
+                it("Changing Value after cloning. Negative Test", function () {
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    actual[1] = "hig";
+                    expect(clone[1]).not.toEqual(actual[1]);
+                });
+
+                it("Splicing 1 item", function () {
+                    var clone = PBDeskJS.Utils.Clone(actual);
+                    actual.splice(1, 1);
+                    expect(clone.length).toBe(2);
+                });
             });
 
         });
